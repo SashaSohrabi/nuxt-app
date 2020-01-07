3eca613996b90a788259d64847ca460336439394
sashasohrabi <shb.sohrabi@gmail.com>
2020-01-07 21:30:03 +0200

    vuex, fetch() and nuxtServerInit()

--- store/index.js
+++ store/index.js
@@ -11,6 +11,36 @@ const createStore = () => {
       }
     },
     actions: {
+      nuxtServerInit(vuexContex, context) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            vuexContex.commit("setPosts", [
+              {
+                id: "1",
+                title: "First Post",
+                previewText: "This is the first post",
+                thumbnail:
+                  "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+              },
+              {
+                id: "2",
+                title: "Second Post",
+                previewText: "This is the second post",
+                thumbnail:
+                  "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+              },
+              {
+                id: "3",
+                title: "Third Post",
+                previewText: "This is the third post",
+                thumbnail:
+                  "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+              }
+            ]);
+            resolve();
+          }, 1000);
+        });
+      },
       setPosts(vuexContex, posts) {
         vuexContex.commit("setPosts", posts);
       }
