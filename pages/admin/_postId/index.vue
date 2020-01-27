067e53b933bf0759dd2b87b34d1b57a41ba7e3bf
sashasohrabi <shb.sohrabi@gmail.com>
2020-01-28 00:12:19 +0200

    editing posts

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,29 +1,45 @@
 <template>
   <div class="admin-post-page">
     <section class="update-form">
-      <AdminPostForm :post="loadedPost" />
+      <AdminPostForm :post="loadedPost" @submit="onSubmitted" />
     </section>
   </div>
 </template>
 
 <script>
 import AdminPostForm from "@/components/Admin/AdminPostForm";
+import axios from "axios";
 
 export default {
   layout: "admin",
   components: {
     AdminPostForm
   },
-  data() {
-    return {
-      loadedPost: {
-        author: "Sasha",
-        title: "My test post",
-        content: "Super amazing, thanks for that!",
-        thumbnailLink:
-          "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-      }
-    };
+  asyncData(context) {
+    return axios
+      .get(
+        `https://nuxt-app-8eb25.firebaseio.com/posts/${context.params.postId}.json`
+      )
+      .then(res => {
+        console.log(res.data);
+        return {
+          loadedPost: res.data
+        };
+      })
+      .catch(e => context.error(e));
+  },
+  methods: {
+    onSubmitted(editedPost) {
+      axios
+        .put(
+          `https://nuxt-app-8eb25.firebaseio.com/posts/${this.$route.params.postId}.json`,
+          editedPost
+        )
+        .then(res => {
+          this.$router.push("/admin");
+        })
+        .catch(e => console.log(e));
+    }
   }
 };
 </script>
