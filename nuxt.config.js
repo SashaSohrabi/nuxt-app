04bf3b390e43135ad90c3cde72d1fa93ab3dcf39
Sasha Sohrabi <sasha@nxte.nl>
2020-01-29 11:55:04 +0200

    adding plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,9 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    "~plugins/core-components.js"
+  ],
   /*
    ** Nuxt.js dev-modules
    */
